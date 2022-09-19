ee97f2ea325a8e7de511fa7f7a3c475bcd924c46
Isaac Rahel <isaac_rahel@live.com>
2022-09-19 13:08:59 -0300

    feat: creating base project

---
+++ theme/index.js
@@ -0,0 +1,11 @@
+export const themes = {
+    default: {
+      title_color: "FFFFFF",
+      icon_color: "FFFFFF",
+      text_color: "FFFFFF",
+      bg_color: "2FC18C",
+      empty_color: "E1E5EB",
+      fill_color: "006DFB",
+    },
+};
+  
